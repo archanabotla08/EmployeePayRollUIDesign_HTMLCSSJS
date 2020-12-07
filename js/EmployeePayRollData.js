d3a3f6541758539b22931a30bea30303bf143970
archanabotla08 <archana.botla@gmail.com>
2020-12-07 17:57:10 +0530

    [Archana] Refactored: Commit some error but resolved

--- js/EmployeePayRollData.js
+++ js/EmployeePayRollData.js
@@ -1,14 +1,5 @@
 class EmployeePayRollData {
 
-    constructor(...params) {
-        this.name = params[0];
-        this.picture = params[1];
-        this.gender = params[2];
-        this.department = params[3];
-        this.salary = params[4];
-        this.startDate = params[5];
-        this.notes = params[6];
-    }
     get id() {
         return this.id;
     }
@@ -21,7 +12,7 @@ class EmployeePayRollData {
     }
 
     set name(name) {
-        let nameRegex = /[A-Z]{1}[a-z]{2,}/;
+        let nameRegex = RegExp['/[A-Z]{1}[a-z]{2,}/'];
         if (nameRegex.test(name))
             this.name = name;
         else
