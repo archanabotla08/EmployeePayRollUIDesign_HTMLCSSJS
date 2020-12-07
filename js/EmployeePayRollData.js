d3612528718d0c850c90ab2f5d108eaf44905769
archanabotla08 <archana.botla@gmail.com>
2020-12-07 16:47:42 +0530

    [Archana] Added: UC1 to add new attribute to class  adding the to string method

--- js/EmployeePayRollData.js
+++ js/EmployeePayRollData.js
@@ -9,13 +9,23 @@ class EmployeePayRollData {
         this.startDate = params[5];
         this.notes = params[6];
     }
+    get id() {
+        return this.id;
+    }
+    set id(id) {
+        this.id = id;
+    }
 
     get name() {
         return this.name;
     }
 
     set name(name) {
-        this.name = name;
+        let nameRegex = /[A-Z]{1}[a-z]{2,}/;
+        if (nameRegex.test(name))
+            this.name = name;
+        else
+            throw "Invalid name";
     }
 
     get picture() {
@@ -54,7 +64,13 @@ class EmployeePayRollData {
     }
 
     set startDate(startDate) {
-        this.startDate = startDate;
+        startDate = startDate.getTime() + (30 * 24 * 60 * 60 * 1000);
+        let today = new Date().getTime() + (30 * 24 * 60 * 60 * 1000);
+        if (today >= startDate) {
+            this.startDate = startDate;
+        } else {
+            ("Invalid date");
+        }
     }
 
     get notes() {
@@ -63,5 +79,11 @@ class EmployeePayRollData {
 
     set notes(notes) {
         this.notes = notes;
+    }
+
+    toString() {
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-US", options);
+        return "id = " + this.id + ", name = " + this.name + ", gender = " + this.gender + ", profile picture = " + this.picture + ", department = " + this.department + ", salary = " + this.salary + ", startDate = " + this.startDate + ", notes = " + this.notes;
     }
 }
