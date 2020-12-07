23e84d3dd5bf2fd2cfa8f3cd0d7e6f7aeb840354
archanabotla08 <archana.botla@gmail.com>
2020-12-07 19:22:49 +0530

    [Archana] Refactored: code for the reset form

---
+++ js/form.js
@@ -0,0 +1,74 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    name.addEventListener('input', function () {
+        if (name.value.length == 0){
+            textError.textContent = "";
+            return;
+        }
+        try{
+            (new EmployeePayRollData()).name = name.value;
+            textError.textContent="";
+        }catch(e){
+            textError.textContent = e ;
+        }
+          
+    });
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener( 'input' ,function () {
+        output.textContent = salary.value;
+    });
+});
+
+const save = () => {
+    try {
+        console.log("hi")
+        let employeePayroll = createEmployeePayroll();
+ 
+    } catch (e) {
+        return
+    }
+}
+
+const createEmployeePayroll = () => {
+    let employeePayroll = new EmployeePayRollData();
+   
+    try {
+        employeePayroll.name = getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error',e);
+        throw e;
+    }
+    employeePayroll.picture = getSelectedValues('[name=profile]').pop();
+    employeePayroll.gender = getSelectedValues('[name=gender]').pop();
+    employeePayroll.department = getSelectedValues('[name=department]');
+    employeePayroll.salary = getInputValueById('#salary');
+    employeePayroll.notes = getInputValueById('#notes');
+    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+    employeePayroll.date = Date.parse(date);
+    alert(employeePayroll.toString);
+    return employeePayroll;
+}
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let setItems = [];
+    allItems.forEach(item => {
+        if (item.checked) setItems.push(item.value);
+    });
+    return setItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id) => {
+    let value = document.getElementById(id).value;
+    return value;
+}
+function resetForm() {
+    document.getElementById("formId").reset();
+}
