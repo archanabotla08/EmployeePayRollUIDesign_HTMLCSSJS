4736b0f827752d5f9a7f1a5519f9b5fd977c765e
archanabotla08 <archana.botla@gmail.com>
2020-12-07 19:59:26 +0530

    [Archana] Added: UC4 to save employee add data to local storage

--- js/EmployeePayRollData.js
+++ js/EmployeePayRollData.js
@@ -1,14 +1,5 @@
 class EmployeePayRollData {
 
-    constructor(...params) {
-        this.name = params[0];
-        this.picture = params[1];
-        this.gender = params[2];
-        this.department = params[3];
-        this.salary = params[4];
-        this.startDate = params[5];
-        this.notes = params[6];
-    }
     get id() {
         return this.id;
     }
@@ -21,11 +12,11 @@ class EmployeePayRollData {
     }
 
     set name(name) {
-        let nameRegex = /^[A-Z]{1}[a-z]{2,}$/;
+        let nameRegex = /^[A-Z][a-z]{2,}$/;
         if (nameRegex.test(name))
             this.name = name;
-        else throw "Invalid name";
-            
+        else
+            throw "Invalid name";
     }
 
     get picture() {
