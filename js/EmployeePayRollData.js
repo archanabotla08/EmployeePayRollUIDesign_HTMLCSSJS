124e22fb1f2cddd90c0e935565cf6470a4f48d88
archanabotla08 <archana.botla@gmail.com>
2020-12-13 16:10:51 +0530

    [Archana] Modified: Some date code

--- js/EmployeePayRollData.js
+++ js/EmployeePayRollData.js
@@ -47,7 +47,7 @@ class EmployeePayRollData {
     //method
     toString() {
         const options = { year: 'numeric', month: 'long', day: 'numeric' };
-     //   const empDate = !this.startDate ? "undefined" : ""+ this.startDate.toLocalDateString("en-US", options);
+      //  const empDate = !this.startDate ? "undefined" : ""+ this.startDate.toLocalDateString("en-US", options);
         return "id=" + this.id + ", name=" + this.name + ", gender=" + this.gender +
             ", profilePic=" + this.profilePic + ", department=" + this.department +
             ", salary=" + this.salary + ", startDate=" + this.startDate + ", note=" + this.note;
